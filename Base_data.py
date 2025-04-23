b10d0d9e59b1b4c7972bb4906aa10ba4d9817e1c
ClementT <168286099+Azila123@users.noreply.github.com>
2025-04-24 01:10:46 +0200

    ajout requirements

--- Base_data.py
+++ Base_data.py
@@ -1,4 +1,22 @@
 import pandas as pd
+import gdown
+import os
+
+# === 1. Télécharger les fichiers Google Drive dans le dossier actuel ===
+fichiers_drive = {
+    "base-cc-emploi-pop-active-2020_v2.CSV": "1ZSzHZwxcsoDn4VxyjsPbj7K0zmoJYPdL",
+    "base-cc-logement-2020.CSV": "1LU57jvjNQSOGwnGM6_cNbENVYez1FKzP",
+    "data-es.csv": "1nn1TT2_2hNXLyDFOlA7StDYLOR0UE3NN"
+}
+
+for nom_fichier, file_id in fichiers_drive.items():
+    if not os.path.exists(nom_fichier):
+        url = f"https://drive.google.com/uc?id={file_id}"
+        print(f"📥 Téléchargement de {nom_fichier}...")
+        gdown.download(url, nom_fichier, quiet=False)
+    else:
+        print(f"✅ {nom_fichier} déjà présent.")
+
 
 # === 1. Population - Chargement et filtrage ===
 df_pop = pd.read_excel("POPULATION_MUNICIPALE_COMMUNES_FRANCE.xlsx")
